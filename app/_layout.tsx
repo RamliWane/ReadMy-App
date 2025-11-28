a825a7408745062539131b6de3ed73336eb3c048
RamliWane <ramlisilawane865@gmail.com>
2025-11-28 22:37:44 +0700

    little change on layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,17 +12,11 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
       <Stack>
-        {/* AUTH */}
         <Stack.Screen name="auth/login" options={{ headerShown: false }} />
         <Stack.Screen name="auth/register" options={{ headerShown: false }} />
-
-        {/* MAIN APP */}
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-        {/* MODAL */}
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
-
       <StatusBar style="auto" />
     </ThemeProvider>
   );
